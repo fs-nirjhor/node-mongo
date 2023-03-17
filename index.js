bede383cbe9c49e39a4d424702adce97e7e3c4df
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-03-17 15:21:36 +0600

    commit

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+	const mobile = {name: 'mobile', quantity: 4, price: 12000};
+	const laptop = {name: "Dell", quantity: 5, price: 25000};
+	const products = [mobile,laptop];
+	
 app.get("/", (req, res) => {
-  res.send("Hello World By FS Nirjhor");
+  res.send(products);
+});
+
+app.get("/laptop", (req, res) => {
+	res.send(laptop);
 });
 
 app.listen(port, () => {
-  console.log(`Farhan's Example app listening on port ${port}`);
+  console.log(`Farhan's node express listening on port ${port}`);
 });
