7a8866ab8e64fa56ef2e219e6220ff3fb43d357c
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-03-18 12:33:45 +0600

    commit

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
 
-	const mobile = {name: 'mobile', quantity: 4, price: 12000};
-	const laptop = {name: "Dell", quantity: 5, price: 25000};
-	const products = [mobile,laptop];
-	
+app.use(cors());
+// parse application/json
+app.use(bodyParser.json());
+
+const mobile = { name: "Xiaomi", price: 12000 };
+const laptop = { name: "Dell", price: 25000 };
+const products = [mobile, laptop];
+
 app.get("/", (req, res) => {
   res.send(products);
 });
 
 app.get("/laptop", (req, res) => {
-	res.send(laptop);
-}); 
+  res.send(laptop);
+});
 
-const users = ["Farhan", "Sadik", 'Nirjhor', 'Jhor', 'Shaila', 'Ferdous', 'Labonno'];
+const users = [
+  "Farhan",
+  "Sadik",
+  "Nirjhor",
+  "Jhor",
+  "Shaila",
+  "Ferdous",
+  "Labonno",
+];
 
 app.get("/users/:id", (req, res) => {
-	console.log(req.query); 
-	//localhost:3000/users/4?sort=name&lang=eng
-	const user = users[req.params.id];
+   // console.log("query", req.query);
+  //localhost:3000/users/4?sort=name&lang=eng
+  const id = req.params.id;
+  const user = users[id];
+  res.send({id, user});
+});
+
+app.post("/addUser", (req, res) => {
+	// save to database 
+	console.log("post", req.body);
+	const user = req.body ;
+	user.id = 5 ;
 	res.send(user);
 });
 
 app.listen(port, () => {
-  console.log(`Farhan's node express listening on port ${port}`);
+  console.log(`Farhan's node listening on port ${port}`);
 });
